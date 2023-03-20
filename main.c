febe048a75142be22a3f38d1b5c88a47fb0e469b
Adam Wilczyński <adam.wilcz7@gmail.com>
2023-03-20 13:14:25 +0100

    commit

--- main.c
+++ main.c
@@ -15,10 +15,6 @@ int is_section_header(const char text[100]) {
     return text[0] == '[' && text[strlen(text) - 1];
 }
 
-int is_comment(const char text[100]) {
-    return text[0] == ';';
-}
-
 int is_key_value_pair(const char line[100]) {
     return first_occurrence_index(line, '=') != strlen(line);
 }
@@ -42,6 +38,11 @@ int is_valid_identifier(const char text[100]) {
     return 1;
 }
 
+struct section {
+    char key_list[100][100];
+    char value_list[100][100];
+};
+
 
 int main(int argc, char *argv[]) {
     char filename[100], selector[100], selector_header_name[100], selector_key[100];
@@ -57,36 +58,46 @@ int main(int argc, char *argv[]) {
     selector_key[characters_to_select_after_separator] = '\0';
 
     printf("Selector: %s\nSelector Header: %s\nSelector Key: %s\n\n", selector, selector_header_name, selector_key);
+
     FILE *file;
-
-    // open file
     file = fopen(filename, "r");
-
     if (file == NULL) {
         printf("Error opening file.");
         return 1;
     }
 
-    // read file line by line
     char line[100];
-    int line_length;
 
     char current_header_name[100];
     char key[100];
     char value[100];
 
-//    struct section_struct[100] section_list[100];
-//    int section_list_index = 0;
-//
-//    struct section_struct current_section[100];
-//    int current_section_index = 0;
     int i;
     int selection_found_flag = 0;
 
-    int equal_sign_index;
+    struct section section_list[100];
+    int section_list_current_index = -1;
+
+    char current_key_list[100][100];
+    int key_list_current_index = 0;
+    char current_value_list[100][100];
+    int value_list_current_index = 0;
+
     while (fgets(line, sizeof(line), file)) {
 //        printf("%s", line);
+
         if (is_section_header(line)) {
+            if (section_list_current_index >= 0) {
+                struct section current_section;
+                memcpy(current_section.key_list, current_key_list, 100 * 100 * sizeof(char));
+                memcpy(current_section.value_list,  current_value_list, 100 * 100 * sizeof(char));
+                section_list[section_list_current_index] = current_section;
+
+                key_list_current_index = 0;
+                value_list_current_index = 0;
+            }
+            ++section_list_current_index;
+
             strcpy(current_header_name, "");
             for (i = 1; i < strlen(line) - 1; ++i) {
                 current_header_name[i - 1] = line[i];
@@ -95,11 +106,10 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
-        equal_sign_index = first_occurrence_index(line, '=');
+        int equal_sign_index = first_occurrence_index(line, '=');
         if (is_key_value_pair(line)) {
             strcpy(key, "");
             strcpy(value, "");
-            int i;
             for (i = 0; i < equal_sign_index - 1; ++i) {
                 key[i] = line[i];
             }
