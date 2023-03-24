698ee226eaf1e2fd3d2edfa9957b2eecb66af6b0
Adam Wilczyński <103942188+EntropistA@users.noreply.github.com>
2023-03-24 21:10:15 +0100

    Add dynamic main

--- main.c
+++ main.c
@@ -1,142 +1,190 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-# include <ctype.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-int first_occurrence_index(const char text[100], char target) {
-    for (int i = 0; i < strlen(text); ++i) {
-        if (text[i] == target) {
+char c;
+int i, start_size, separator_index;
+
+struct {
+    char *char_array;
+    int size;
+} typedef str;
+
+bool is_proper_identifier(str s) {
+    for (i = 0; i < s.size - 1; ++i) {
+        if (!isalnum(s.char_array[i]) & s.char_array[i] != '-') {
+            return false;
+        }
+    }
+    return true;
+}
+
+int first_index(const str s, const char target) {
+    for (i = 0; i < strlen(s.char_array); ++i) {
+        if (s.char_array[i] == target) {
             return i;
         }
     }
-    return strlen(text);
+    return s.size;
 }
 
-int is_section_header(const char text[100]) {
-    return text[0] == '[' && text[strlen(text) - 1];
-}
-
-int is_key_value_pair(const char line[100]) {
-    return first_occurrence_index(line, '=') != strlen(line);
-}
-
-int is_numerical(const char text[100]) {
-    for (int i = 0; i < strlen(text); ++ i) {
-        if (!isdigit(text[i])) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-int is_valid_identifier(const char text[100]) {
-    for (int i = 0; i < strlen(text); ++i) {
-        char c = text[i];
-        if (!isalnum(c) && c != '-') {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-struct section {
-    char key_list[100][100];
-    char value_list[100][100];
+bool is_key_value_pair(const str line) {
+    return first_index(line, '=') != line.size;
 };
+
+str extract_str(str line, int start, int end) {
+    str result;
+    result.size = end - start + 1;
+    result.char_array = malloc(sizeof(char) * result.size);  // on assumption that a space separates '=' from both sides
+    strncpy(result.char_array, line.char_array + start, result.size - 1);
+    result.char_array[result.size - 1] = '\0';
+    return result;
+}
+
+bool is_header(const str line) {
+    return line.char_array[0] == '[';
+}
+
+str get_header(const str line) {
+    str header;
+    header.size = strlen(line.char_array) - 2 + 1;
+    header.char_array = malloc(sizeof(char) * header.size);
+
+    for (i = 1; i < strlen(line.char_array) - 1; ++i) {
+        header.char_array[i - 1] = line.char_array[i];
+    }
+    header.char_array[i - 1] = '\0';
+    return header;
+}
+
+struct {
+    str section;
+    str key;
+    str value;
+} typedef item;
+
+void print_item(const item item_to_print) {
+    printf("[%s] %s = %s\n", item_to_print.section.char_array, item_to_print.key.char_array, item_to_print.value.char_array);
+}
+
+struct {
+    item *item_array;
+    int size;
+} typedef dynamic_item_array;
+
+int find_first_item_with_section(dynamic_item_array database, str section) {
+    item current_item;
+    for (i = 0; i < database.size; ++i) {
+        current_item = database.item_array[i];
+        if (strcmp(current_item.section.char_array, section.char_array) == 0) {
+            return i;
+        }
+    }
+    return database.size;
+}
+
+int find_item_index_by_key_and_value(dynamic_item_array database, int section_start_index, str key) {
+    item current_item = database.item_array[section_start_index];
+    str start_section = current_item.section;
+    for (i = section_start_index; i < database.size & strcmp(current_item.section.char_array, start_section.char_array) == 0; ++i) {
+        current_item = database.item_array[i];
+        if (strcmp(current_item.key.char_array, key.char_array) == 0) {
+            return i;
+        }
+    }
+    return database.size;
+}
+
 
 
 int main(int argc, char *argv[]) {
-    char filename[100], selector[100], selector_header_name[100], selector_key[100];
-    strcpy(filename, argv[1]);
-    strcpy(selector, argv[2]);
-    printf("File: %s\n", filename, selector);
-
-    int selector_separator_index = first_occurrence_index(selector, '.');
-    strncpy(selector_header_name, selector, selector_separator_index);
-    selector_header_name[selector_separator_index] = '\0';
-    int characters_to_select_after_separator = strlen(selector) - selector_separator_index - 1;
-    strncpy(selector_key, selector + selector_separator_index + 1, characters_to_select_after_separator);
-    selector_key[characters_to_select_after_separator] = '\0';
-
-    printf("Selector: %s\nSelector Header: %s\nSelector Key: %s\n\n", selector, selector_header_name, selector_key);
-
     FILE *file;
-    file = fopen(filename, "r");
+    file = fopen(argv[1], "r");
+
     if (file == NULL) {
-        printf("Error opening file.");
+        printf("Error occurred when opening a file: %s", argv[1]);
         return 1;
     }
 
-    char line[100];
+    str line;
+    start_size = 1;
+    line.char_array = malloc(sizeof(char) * start_size);
+    line.char_array[0] = '\0';
+    line.size = start_size;
 
-    char current_header_name[100];
-    char key[100];
-    char value[100];
+    dynamic_item_array database;
+    database.item_array = malloc(sizeof(item));
+    database.size = 0;
 
-    int i;
-    int selection_found_flag = 0;
+    str current_header;
+    item current_line_item;
+    while ((c = fgetc(file)) != EOF) {
+        if (c == '\n') {
+//            printf("%d %s\n", is_header(line), line.char_array);
+            if (is_header(line)) {
+                current_header = get_header(line);
+            } else if (is_key_value_pair(line)) {
+                separator_index = first_index(line, '=');
+                current_line_item.key = extract_str(line, 0, first_index(line, '=') - 1);
+                current_line_item.value = extract_str(line, separator_index + 2, strlen(line.char_array));
+                current_line_item.section = current_header;
 
-    struct section section_list[100];
-    int section_list_current_index = -1;
-
-    char current_key_list[100][100];
-    int key_list_current_index = 0;
-    char current_value_list[100][100];
-    int value_list_current_index = 0;
-
-    while (fgets(line, sizeof(line), file)) {
-//        printf("%s", line);
-
-        if (is_section_header(line)) {
-            if (section_list_current_index >= 0) {
-                struct section current_section;
-                memcpy(current_section.key_list, current_key_list, 100 * 100 * sizeof(char));
-                memcpy(current_section.value_list,  current_value_list, 100 * 100 * sizeof(char));
-                section_list[section_list_current_index] = current_section;
-
-                key_list_current_index = 0;
-                value_list_current_index = 0;
-            }
-            ++section_list_current_index;
-
-            strcpy(current_header_name, "");
-            for (i = 1; i < strlen(line) - 1; ++i) {
-                current_header_name[i - 1] = line[i];
-            }
-            current_header_name[i - 2] = '\0';
-            continue;
-        }
-
-        int equal_sign_index = first_occurrence_index(line, '=');
-        if (is_key_value_pair(line)) {
-            strcpy(key, "");
-            strcpy(value, "");
-            for (i = 0; i < equal_sign_index - 1; ++i) {
-                key[i] = line[i];
-            }
-            key[i] = '\0';
-            for (i = 0; i < strlen(line) - (equal_sign_index + 2); ++i) {
-                value[i] = line[i + equal_sign_index + 2];
-            }
-            value[i] = '\0';
-
-//            printf("Line: %sKey: %s\nValue: %s\n\n", line, key, value);
-//            printf("Current: %s Selector: %s\n", current_header_name, selector_header_name);
-            if (strcmp(current_header_name, selector_header_name) == 0) {
-                selection_found_flag = 1;
-                if (strcmp(key, selector_key) == 0) {
-                    printf("Value found: %s\n", value);
-                    return 0;
+                if (!is_proper_identifier(current_line_item.key)) {
+                    printf("%s is an invalid identifier", current_line_item.key.char_array);
+                    return -1;
+                } else if (!is_proper_identifier(current_line_item.value)) {
+                    printf("%s is an invalid identifier", current_line_item.value.char_array);
+                    return -1;
+                } else if (!is_proper_identifier(current_line_item.section)) {
+                    printf("%s is an invalid identifier", current_line_item.section.char_array);
+                    return -1;
                 }
+
+                database.item_array = realloc(database.item_array, sizeof(item) * ++database.size);
+                database.item_array[database.size - 1] = current_line_item;
             }
+
+//            printf("%s\n",line.char_array);
+            line.size = 1;
+            line.char_array[line.size - 1] = '\0';
+        } else if (strlen(line.char_array) + 2 >= line.size) {
+            line.char_array = realloc(line.char_array, sizeof(char) * ++line.size);
+            line.char_array[line.size - 2] = c;
+            line.char_array[line.size - 1] = '\0';
         }
     }
-    fclose(file);
 
-    if (!selection_found_flag) {
-        printf("Failed to find section %s", selector_header_name);
-        return 1;
+    if (strcmp(argv[2], "expression") != 0) {
+        // Search Query
+        str selector;
+        selector.char_array = argv[2];
+        selector.size = strlen(selector.char_array) + 1;
+
+        separator_index = first_index(selector, '.');
+        str section = extract_str(selector, 0, separator_index);
+        str key = extract_str(selector, separator_index + 1, selector.size);
+
+        printf("%s %s\n", section.char_array, key.char_array);
+
+        int section_index = find_first_item_with_section(database, section);
+        if (section_index == database.size) {
+            printf("Failed to find section [%s]", section.char_array);
+            return -1;
+        }
+
+        int item_index = find_item_index_by_key_and_value(database, section_index, key);
+        if (item_index == database.size) {
+            printf("Failed to find key \"%s\" in section [%s]", key.char_array, section.char_array);
+            return -1;
+        }
+        print_item(database.item_array[item_index]);
     } else {
-        printf("Failed to find key %s in section %s", selector_key, selector_header_name);
-        return 1;
+        // operation_query
+        printf("%s", argv[3]);
     }
+
+
+    return 0;
 }
