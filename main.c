c28288fb54cab8125f9b0954e35fb43c6b81364b
Adam Wilczyński <adam.wilcz7@gmail.com>
2023-03-26 19:55:32 +0200

    Working 5.0 version

--- main.c
+++ main.c
@@ -12,6 +12,15 @@ struct {
     int size;
 } typedef str;
 
+bool is_string_numerical(str s) {
+    for (i = 0; i < s.size - 1; ++i) {
+        if (!isdigit(s.char_array[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
 bool is_proper_identifier(str s) {
     for (i = 0; i < s.size - 1; ++i) {
         if (!isalnum(s.char_array[i]) & s.char_array[i] != '-') {
@@ -34,11 +43,11 @@ bool is_key_value_pair(const str line) {
     return first_index(line, '=') != line.size;
 };
 
-str extract_str(str line, int start, int end) {
+str extract_str(str s, int start, int end) {
     str result;
     result.size = end - start + 1;
     result.char_array = malloc(sizeof(char) * result.size);  // on assumption that a space separates '=' from both sides
-    strncpy(result.char_array, line.char_array + start, result.size - 1);
+    strncpy(result.char_array, s.char_array + start, result.size - 1);
     result.char_array[result.size - 1] = '\0';
     return result;
 }
@@ -97,6 +106,64 @@ int find_item_index_by_key_and_value(dynamic_item_array database, int section_st
     return database.size;
 }
 
+int find_item_index(str selector, dynamic_item_array database) {
+    separator_index = first_index(selector, '.');
+    str section = extract_str(selector, 0, separator_index);
+    str key = extract_str(selector, separator_index + 1, selector.size);
+
+    int section_index = find_first_item_with_section(database, section);
+    if (section_index == database.size) {
+        printf("Failed to find section [%s]\n", section.char_array);
+        return -1;
+    }
+
+    int item_index = find_item_index_by_key_and_value(database, section_index, key);
+    if (item_index == database.size) {
+        printf("Failed to find key \"%s\" in section [%s]\n", key.char_array, section.char_array);
+        return -2;
+    }
+    return item_index;
+}
+
+str process_expression(str a, str b, char operator) {
+    str result;
+
+    if (is_string_numerical(a) & is_string_numerical(b)) {
+        long long int a_numerical, b_numerical;
+        sscanf(a.char_array, "%lld", &a_numerical);
+        sscanf(b.char_array, "%lld", &b_numerical);
+        switch (operator) {
+            case '+':
+                sprintf(result.char_array, "%lli", a_numerical + b_numerical);
+                break;
+            case '-':
+                sprintf(result.char_array, "%lli", a_numerical - b_numerical);
+                break;
+            case '*':
+                sprintf(result.char_array, "%lli", a_numerical * b_numerical);
+                break;
+            case '/':
+                sprintf(result.char_array, "%lli", a_numerical / b_numerical);
+                break;
+            default:
+                printf("Invalid operator %c", operator);
+                result.size = -1;
+                return result;
+        }
+        result.size = strlen(result.char_array);
+        return result;
+    }
+
+    if ((operator == '+') & !is_string_numerical(a) & !is_string_numerical(b)) {
+        result.size = a.size + b.size - 1;
+        result.char_array = malloc(result.size);
+        strncpy(result.char_array , a.char_array, a.size - 1);
+        strncpy(result.char_array + a.size - 1, b.char_array, b.size);
+        return result;
+    }
+
+    printf("Invalid operation: %s %c %s", a.char_array, operator, b.char_array);
+}
 
 
 int main(int argc, char *argv[]) {
@@ -162,28 +229,44 @@ int main(int argc, char *argv[]) {
         selector.char_array = argv[2];
         selector.size = strlen(selector.char_array) + 1;
 
-        separator_index = first_index(selector, '.');
-        str section = extract_str(selector, 0, separator_index);
-        str key = extract_str(selector, separator_index + 1, selector.size);
-
-        printf("%s %s\n", section.char_array, key.char_array);
-
-        int section_index = find_first_item_with_section(database, section);
-        if (section_index == database.size) {
-            printf("Failed to find section [%s]", section.char_array);
-            return -1;
-        }
-
-        int item_index = find_item_index_by_key_and_value(database, section_index, key);
-        if (item_index == database.size) {
-            printf("Failed to find key \"%s\" in section [%s]", key.char_array, section.char_array);
+        int item_index = find_item_index(selector, database);
+        if (item_index < 0) {
             return -1;
         }
         print_item(database.item_array[item_index]);
     } else {
-        // operation_query
-        printf("%s", argv[3]);
+        // Query
+        str query, a, b;
+        query.char_array = argv[3];
+        query.size = strlen(argv[3]);
+
+        separator_index = first_index(query, ' ');
+        if (separator_index == query.size) {
+            printf("Invalid expression");
+            return -1;
+        }
+
+        a = extract_str(query, 0, separator_index);
+        char operator = query.char_array[separator_index + 1];
+        b = extract_str(query, separator_index + 3, query.size);
+
+        int a_index, b_index;
+        a_index = find_item_index(a, database);
+        b_index = find_item_index(b, database);
+        if (a_index < 0 | b_index < 0) {
+            return -1;
+        }
+
+        str a_value, b_value;
+        a_value = database.item_array[a_index].value;
+        b_value = database.item_array[b_index].value;
+        str result = process_expression(a_value, b_value, operator);
+        if (result.size < 0) {
+            return -1;
+        }
+        printf("%s %c %s = %s", a_value.char_array, operator, a_value.char_array, result.char_array);
     }
+
 
 
     return 0;
